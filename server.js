76202009d58afa9342de18befd30980ab20390f3
Storey Sheldon <storey.e.sheldon@gmail.com>
2020-03-30 10:40:12 -0500

    working signup with sql

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use(passport.session());
 
 // Requiring our routes
 require("./routes/api-routes.js")(app);
-// require("./routes/html-routes.js")(app);
-// require("./routes/podcast-routes.js")(app);
+require("./routes/html-routes.js")(app);
+require("./routes/podcast-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync({}).then(function () {
